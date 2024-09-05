c0e5fb3950bd4a208928fa24045c0152ffe2d888
Anubhav <choubey.anubhav253@gmail.com>
2024-09-05 16:07:56 +0530

    Tasks need to do

--- backend/functions.py
+++ backend/functions.py
@@ -1,1 +1,8 @@
 # Now deadline 9
+
+# Tasks in hand :
+# 1. Need to create OCR function
+# 2. Photo shop / Edit detector
+# 3. Aadhar basic API
+# 4. File encrypt and decrypt function  
+# 5. Need to design a database
