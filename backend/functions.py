144ab88de14eb39a8bda57a24f013c65ac104a21
Anubhav <choubey.anubhav253@gmail.com>
2024-09-06 12:22:05 +0530

    Starting work with OCR_Docs

--- backend/functions.py
+++ backend/functions.py
@@ -6,3 +6,11 @@
 # 3. Aadhar basic API
 # 4. File encrypt and decrypt function  
 # 5. Need to design a database
+
+#Importing Libraries
+
+
+
+#Lets Start with OCR Function for documents
+def ocr_docs():
+    pass
