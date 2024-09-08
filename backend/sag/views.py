9deba415164e3f7ceeecf7b467924bf22f992713
Anubhav <choubey.anubhav253@gmail.com>
2024-09-08 21:11:13 +0530

    Started working URL

--- backend/sag/views.py
+++ backend/sag/views.py
@@ -3,8 +3,43 @@ from django.http import JsonResponse
 import json
 from backend.models import SAGBureau, Students
 from django.views.decorators.csrf import csrf_exempt
+from functions import analyze_noise, extract_exif
+import requests
+from io import BytesIO
 
 # Create your views here.
+from PIL import Image
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+# Configure Cloudinary
+cloudinary.config(
+  cloud_name='dm23rhuct',  # Replace with your Cloudinary cloud name
+  api_key='972235453857764',        # Replace with your Cloudinary API key
+  api_secret='ZssdWvtjhYTH3ULILEf_ZYVK5zc'   # Replace with your Cloudinary API secret
+)
+
+def download_and_return_url(image_url: str):
+    response = requests.get(image_url)
+    img = Image.open(BytesIO(response.content))
+
+    img.save('temper.jpg')
+    analyze_noise('temper.jpg')
+
+    meta_res = extract_exif('temper.jpg')
+
+    img = Image.open('detected_edit.jpg')
+
+    img_bytes = BytesIO()
+    img.save(img_bytes, format='JPEG')
+    img_bytes.seek(0)
+
+    response = cloudinary.uploader.upload(img_bytes, public_id=f'bosad_10th')
+    file_url = response['secure_url']
+
+    return meta_res,file_url
+
 
 @csrf_exempt    
 def login(request):
